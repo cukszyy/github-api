a019949a566ff6c3a62603cbd7223153ce89a93a
Lucas Portella <lucas.portella>
2020-09-12 20:48:00 -0300

    Initial Commit

---
+++ swiftui-github-api/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-github-api
+//
+//  Created by Lucas Portella on 12/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-github-api/swiftui_github_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_github_apiApp.swift
+//  swiftui-github-api
+//
+//  Created by Lucas Portella on 12/09/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_github_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
